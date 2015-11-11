660d614aa902b2e20d18a2198f617766e59bef6c
Austen <rawleag@gmail.com>
2015-11-11 12:05:25 -0800

    added notes on tests to keep myself organized

--- spec/models/polish_spec.rb
+++ spec/models/polish_spec.rb
@@ -1,34 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Polish, type: :model do
+  #brand
   it { is_expected.to respond_to :brand}
   describe '#brand' do
     subject(:invalid_brand) {Polish.create(brand:"")}
     it { is_expected.to_not be_valid}
   end
-
+  #collection
   it { is_expected.to respond_to :collection}
   describe '#collection' do
     subject(:invalid_collection) {Polish.create(collection:"")}
     it { is_expected.to_not be_valid}
   end
-
+  #release year
   it { is_expected.to respond_to :releaseYear}
-
+  #name
   it { is_expected.to respond_to :name}
   describe '#name' do
     subject(:invalid_name) {Polish.create(name:"")}
     it { is_expected.to_not be_valid}
   end
-
+  #id code
   it { is_expected.to respond_to :code}
-
+  #finish
   it { is_expected.to respond_to :finish}
   describe '#finish' do
     subject(:invalid_finish) {Polish.create(finish:"")}
     it { is_expected.to_not be_valid}
   end
-
+  #color
   it { is_expected.to respond_to :color}
   describe '#color' do
     subject(:invalid_color) {Polish.create(color:"")}
