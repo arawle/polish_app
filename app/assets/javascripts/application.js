e74e9f0f0ac2e8427335a35b9fdf9b1b713bf0b9
Austen <rawleag@gmail.com>
2015-11-11 21:35:46 -0800

    bootstrap sprockets required

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require angular/lib/angular.min
 //= require_tree ./angular/lib
 //= require angular/polishes/app
