3dd1eaa0e891b64643797cf0295666f31af74e52
Austen <rawleag@gmail.com>
2015-11-12 09:32:12 -0800

    polish controller and comments for what it needs

---
+++ app/assets/javascripts/angular/polishes/polishController.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('myapp')
+    .controller('polishController', polishController);
+
+  polishController.$inject = [
+    '$scope',
+    '$location',
+    '$routeParams'
+    //factories
+  ];
+
+  function polishesController($scope, $location, $routeParams){
+
+  //show polish
+
+  //add polish
+
+  //edit polish(admin only)
+
+  //delete polish(admin only)
+
+  //go back to all polishes
+
+  //go back to brand page
+
+
+  }
+})();
