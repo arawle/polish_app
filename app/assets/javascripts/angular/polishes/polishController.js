3f4067de290ca189794b6b7029c5f80e77d34d99
Austen <rawleag@gmail.com>
2015-11-12 16:46:37 -0800

    delete polish works

--- app/assets/javascripts/angular/polishes/polishController.js
+++ app/assets/javascripts/angular/polishes/polishController.js
@@ -14,13 +14,15 @@
 
   function polishController($scope, $location, $routeParams, ResourceFactory){
     $scope.polish = ResourceFactory.get({id: $routeParams.id});
-
     //update polish(admin only)
     $scope.updatePolish = function (polish){
       $scope.polish.$update({id: polish.id})
     };
     //delete polish(admin only)
-
+    $scope.deletePolish = function (polish){
+      ResourceFactory.delete({id: polish.id})
+      $location.path('/polishes');
+    }
     //go back to all polishes
 
     //go back to brand page
