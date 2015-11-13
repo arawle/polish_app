80c64a4ac3f7fc493543528ebf400cd1ef0c25ec
Austen <rawleag@gmail.com>
2015-11-12 16:09:47 -0800

    removed excess comments

--- app/assets/javascripts/angular/polishes/polishesController.js
+++ app/assets/javascripts/angular/polishes/polishesController.js
@@ -13,14 +13,9 @@
   ];
 
   function polishesController($scope, $location, PolishesFactory, ResourceFactory){
-    // PolishesFactory.allPolish()
-    // $scope.allPolish = PolishesFactory.polishes;
     $scope.allPolishes = ResourceFactory.query()
     $scope.polish = {}
 
-    //show polishes
-
-    //add polish
     $scope.add = function (polish){
       ResourceFactory.save(polish)
       $scope.polish = {};
