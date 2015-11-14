24edcf7803edd3c44ab4429ef4e91929e2e120a9
Austen <rawleag@gmail.com>
2015-11-13 18:09:43 -0800

    added 2nd resource factory with goal of being able to add states to polishes

--- app/assets/javascripts/angular/polishes/ResourceFactory.js
+++ app/assets/javascripts/angular/polishes/ResourceFactory.js
@@ -1,6 +1,9 @@
 var app = angular.module("myapp");
 
 app.factory('ResourceFactory', ['$resource', function($resource) {
-  console.log('here')
   return $resource('/polishes/:id.json', {}, {update:{method:'PUT'}})
+}]);
+
+app.factory('StateResourceFactory', ['$resource', function($resource) {
+  return $resource('/polishes/:polishes_id/states/:id.json', {}, {update:{method:'PUT'}})
 }]);
