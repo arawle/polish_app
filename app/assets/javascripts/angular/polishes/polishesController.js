b1661b5ef0ef9a57250f7878addb6e2a1d97fca5
Austen <rawleag@gmail.com>
2015-11-16 11:36:20 -0800

    working on file uploads

--- app/assets/javascripts/angular/polishes/polishesController.js
+++ app/assets/javascripts/angular/polishes/polishesController.js
@@ -8,15 +8,33 @@
   polishesController.$inject = [
     '$scope',
     '$location',
+    '$timeout',
     'PolishesFactory',
-    'ResourceFactory'
+    'ResourceFactory',
+    'Upload'
   ];
 
-  function polishesController($scope, $location, PolishesFactory, ResourceFactory){
+  function polishesController($scope, $location, $timeout, PolishesFactory, ResourceFactory, Upload){
     $scope.allPolishes = ResourceFactory.query()
     $scope.polish = {}
 
+
+    // $scope.uploadFile = function (file, errFile){
+    //   $scope.errFile = errFiles && errFiles[0];
+    //   if (file) {
+    //     file.upload = Upload.upload({
+    //       url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+    //       data: {file: file}
+    //     }).then(function (response) {
+    //       $timeout(function () {
+    //         file.result = response.data;
+    //       });
+    //     }
+    //   }
+    // };
+
     $scope.add = function (polish){
+      $scope.polish.picture = $scope.uploadFile(polish.picture);
       ResourceFactory.save(polish)
       $scope.polish = {};
       $scope.allPolishes = ResourceFactory.query();
