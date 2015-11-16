4ce5cdd48aa9fde9238c44c947f596406e784778
Austen <rawleag@gmail.com>
2015-11-16 12:03:11 -0800

    correct column name photo -> picture

--- app/models/polish.rb
+++ app/models/polish.rb
@@ -2,7 +2,7 @@ class Polish < ActiveRecord::Base
   validates_presence_of :brand, :collection, :name, :finish, :color
   has_many :states
 
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 
 end
