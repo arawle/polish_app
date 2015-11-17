e56894c9b3699fc77ea985e0e627bcffcccbab8d
Austen <rawleag@gmail.com>
2015-11-16 16:56:31 -0800

    picture upload

--- app/models/polish.rb
+++ app/models/polish.rb
@@ -2,7 +2,7 @@ class Polish < ActiveRecord::Base
   validates_presence_of :brand, :collection, :name, :finish, :color
   has_many :states
 
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :picture
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 
 end
