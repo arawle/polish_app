f3d7304255cb5c39c2f496f1522191b27cb19c0c
Austen <rawleag@gmail.com>
2015-11-18 14:18:37 -0800

    add heroku

--- app/assets/javascripts/angular/polishes/polishFactory.js
+++ app/assets/javascripts/angular/polishes/polishFactory.js
@@ -32,7 +32,7 @@
     PolishFactory.createStateWithAttachment = function(polishId, formData){
       console.log(polishId, formData)
       var deferred = $q.defer();
-      var url = "//localhost:3000/polishes/" + polishId + "/states";
+      var url = "//polish.herokuapp.com/polishes/" + polishId + "/states";
       sendState(formData, "POST", url).then(function(data){
         deferred.resolve(data);
       });
