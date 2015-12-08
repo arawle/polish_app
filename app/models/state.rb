8d4fc59d6db102b7c86c350a01d9c3053038c74e
Austen <rawleag@gmail.com>
2015-12-08 11:02:55 -0800

    removed line that kept datetime from working

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,6 +1,5 @@
 class State < ActiveRecord::Base
   validates_presence_of :swatched, :fill, :thinned
-  validates_inclusion_of :box, :original_owner, in: [true, false]
 
   has_attached_file :bottleshot
   validates_attachment_content_type :bottleshot, content_type: /\Aimage\/.*\Z/
