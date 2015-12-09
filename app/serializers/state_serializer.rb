b26ef7d445ad8058b17c7c13620787700d260ed1
Austen <rawleag@gmail.com>
2015-12-09 15:22:51 -0800

    custom serializer for State

---
+++ app/serializers/state_serializer.rb
@@ -0,0 +1,40 @@
+class StateSerializer < ActiveModel::Serializer
+  attributes %i(
+    condition
+    swatched
+    fill
+    thinned
+    notes
+    bottle_shot
+    mani_shot
+    obtained
+    purchase_price
+    size
+    bottleshot
+    blog
+    box
+    original_owner
+    for_sale
+    for_swap
+  )
+
+  def blog
+    object.blog_at ? 'yes' : 'no'
+  end
+
+  def box
+    object.box_at ? 'yes' : 'no'
+  end
+
+  def original_owner
+    object.original_owner_at ? 'yes' : 'no'
+  end
+
+  def for_sale
+    object.for_sale_at ? 'yes' : 'no'
+  end
+
+  def for_swap
+    object.for_swap_at ? 'yes' : 'no'
+  end
+end
