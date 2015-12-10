4138abc7c0250d8bd78b4abfa40a993ca3c5b5b8
Austen <rawleag@gmail.com>
2015-12-10 15:51:42 -0800

    user routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'statics#index'
 
+  resources :users, only: [:show, :create, :update, :destroy]
+
   resources :polishes,  only: [:index, :show, :create, :update, :destroy] do
     resources :states, only: [:index, :show, :create, :update, :destroy]
   end
