1d426b00d77fdfbc4c428d76507bca542babc078
Austen <rawleag@gmail.com>
2015-12-10 14:28:02 -0800

    add user model

---
+++ db/migrate/20151210221831_add_users_to_app.rb
@@ -0,0 +1,14 @@
+class AddUsersToApp < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+      t.string :password_digest
+      t.string :avatar_url
+      t.datetime :admin
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209210432) do
+ActiveRecord::Schema.define(version: 20151210221831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,17 @@ ActiveRecord::Schema.define(version: 20151209210432) do
     t.datetime "for_sale_at"
     t.datetime "for_swap_at"
     t.decimal  "size"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password"
+    t.string   "password_digest"
+    t.string   "avatar_url"
+    t.datetime "admin"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
