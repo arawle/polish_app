d19438e949c69089a8f454349ce9bd2daf578cd3
Austen <rawleag@gmail.com>
2015-12-11 09:46:01 -0800

    check if user logged in or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-
-
-
-
   protect_from_forgery with: :exception
+
+  def confirm_logged_in
+    unless session[:user_id]
+      redirect_to login_path, alert: "Please log in"
+    end
+  end
+
+  def prevent_login_signup
+    if session[:user_id]
+      redirect_to :back, notice: "You are already logged in"
+    end
+  end
 end
