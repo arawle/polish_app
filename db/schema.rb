fce0c2d4adf3e217cb9b60b340212e7382cd571f
Austen <rawleag@gmail.com>
2015-12-10 17:10:07 -0800

    added paperclip and aws required fields to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211004658) do
+ActiveRecord::Schema.define(version: 20151211010903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,8 +76,12 @@ ActiveRecord::Schema.define(version: 20151211004658) do
     t.string   "password_digest"
     t.string   "avatar_url"
     t.datetime "admin"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_foreign_key "collections", "polishes"
