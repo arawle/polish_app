590c6f48d07c4b7a56711c587f60c12a9af0fd84
Austen <rawleag@gmail.com>
2015-12-11 10:33:11 -0800

    user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :current_user
+  helper_method :current_user
 
   def confirm_logged_in
     unless session[:user_id]
@@ -9,9 +11,15 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
   def prevent_login_signup
     if session[:user_id]
       redirect_to :back, notice: "You are already logged in"
     end
   end
+
 end
