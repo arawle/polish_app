aaaca187662f17b381d5be46c0bc4d789ef5d7d5
Austen <rawleag@gmail.com>
2015-12-11 09:39:39 -0800

    User uniqueness validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  validates :username, presence: :true, length: {minimum: 2}
-  validates :email, presence: :true
+  validates :username, presence: :true, uniqueness: true, length: {minimum: 2}
+  validates :email, presence: :true, uniqueness: true
 
   has_many :collections, dependent: :destroy
   has_many :polishes, through: :collections
