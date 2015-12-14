c8fd0fb1f9116c4a15f216d857d23347a7fc6c14
Austen <rawleag@gmail.com>
2015-12-14 15:14:18 -0800

    indentation fix

--- app/assets/javascripts/angular/navbar/navController.js
+++ app/assets/javascripts/angular/navbar/navController.js
@@ -22,17 +22,17 @@
       return $scope.user = user;
     });
 
-  $scope.$on('devise:new-registration', function (e, user){
-    $scope.user = user;
-  });
+    $scope.$on('devise:new-registration', function (e, user){
+      $scope.user = user;
+    });
 
-  $scope.$on('devise:login', function (e, user){
-    $scope.user = user;
-  });
+    $scope.$on('devise:login', function (e, user){
+      $scope.user = user;
+    });
 
-  $scope.$on('devise:logout', function (e, user){
-    $scope.user = {};
-  });
+    $scope.$on('devise:logout', function (e, user){
+      $scope.user = {};
+    });
 
   }
 })();
