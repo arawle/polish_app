b365d383394cbd30a121af0b3fd06cf79bd0d28e
Austen <rawleag@gmail.com>
2016-01-10 15:34:16 -0800

    added a line break

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   def intercept_html_requests
     redirect_to('/') if request.format == Mime::HTML
   end
+
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
   end
