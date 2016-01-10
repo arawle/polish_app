dfa9c9f60607abcb844ea1b9e2e1949bab00c7d4
Austen <rawleag@gmail.com>
2016-01-10 15:33:16 -0800

    return json for collections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
 
   def show
     @user = User.find(params[:id])
+    @collection = Collection.find_by user_id: current_user.id
+
+    render json: @collection, status: :ok
   end
 
 end
